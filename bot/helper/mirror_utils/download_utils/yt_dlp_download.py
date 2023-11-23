eec357cbf11186675b7484f9da401308594ef7c4
Dawn India <z-mirror@dawn-in.eu.org>
2023-11-24 02:12:44 +0530

    Minor fixes
    - Fix anytree
    - Added pCloud support

--- bot/helper/mirror_utils/download_utils/yt_dlp_download.py
+++ bot/helper/mirror_utils/download_utils/yt_dlp_download.py
@@ -198,7 +198,7 @@ class YoutubeDLHelper:
             self.opts['ignoreerrors'] = True
             self.is_playlist = True
 
-        self.__gid = token_urlsafe(10)
+        self.__gid = token_urlsafe(6)
 
         await self.__onDownloadStart()
 
